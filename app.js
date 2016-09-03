aab54123eec564fe80dda62ad83349a09be1d65f
Geng Li <gli@internal.appbridge.io>
2016-09-02 17:21:43 -0700

    first

---
+++ app.js
@@ -0,0 +1,66 @@
+var CLIENT_ID = '329290453260-nkl4lfkduqkh8nfgvm5lr64cffo40o7n.apps.googleusercontent.com';
+var SCOPES = ['https://www.googleapis.com/auth/drive.metadata'];
+
+var filePicker = angular.module('filePicker', []);
+filePicker.controller('mainController', function($scope){
+  $scope.files = [];
+  function checkAuth() {
+    gapi.auth.authorize(
+      {
+        'client_id': CLIENT_ID,
+        'scope': SCOPES.join(' '),
+        'immediate': true
+      }, handleAuthResult);
+    }
+
+    function handleAuthResult(authResult) {
+      // console.log("handleAuthResult");
+      if (authResult && !authResult.error) {
+        loadDriveApi();
+      }
+    }
+
+    function loadDriveApi() {
+      // console.log("loadDriveApi")
+      gapi.client.load('drive', 'v3', listFiles);
+    }
+  $scope.handleAuthClick = function(event) {
+    // console.log("handleAuthClick");
+    gapi.auth.authorize(
+      {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
+      handleAuthResult
+    );
+    return false;
+  }
+  function listFiles() {
+    var request = gapi.client.drive.files.list({
+      'pageSize': 10,
+      'fields': "nextPageToken, files"
+    });
+
+    request.execute(function(resp) {
+      var files = resp.files;
+      for (var i in files) {
+        $scope.files.push(files[i]);
+      }
+      $scope.nextPageToken = resp.nextPageToken;
+      // console.log(files);
+      // console.log($scope.files);
+    });
+  }
+  $scope.nextPage = function(nextPageToken){
+    var request = gapi.client.drive.files.list({
+      'pageSize': 10,
+      'pageToken': nextPageToken,
+      'fields': "nextPageToken, files"
+    });
+    request.execute(function(resp) {
+      $scope.nextPageToken = resp.nextPageToken;
+      var files = resp.files;
+      for (var i in files) {
+        $scope.files.push(files[i]);
+      }
+    })
+  }
+
+})
