43a6b4e9835c4f7165af840bdd4a1c48b61136a1
lg901127 <lg901127@gmail.com>
2016-09-04 19:01:04 -0400

    upload feature

--- app.js
+++ app.js
@@ -13,17 +13,6 @@ filePicker.controller('mainController', function($scope){
       }, handleAuthResult);
     }
 
-    function handleAuthResult(authResult) {
-      // console.log("handleAuthResult");
-      if (authResult && !authResult.error) {
-        loadDriveApi();
-      }
-    }
-
-    function loadDriveApi() {
-      // console.log("loadDriveApi")
-      gapi.client.load('drive', 'v3', listFiles);
-    }
   $scope.handleAuthClick = function(event) {
     // console.log("handleAuthClick");
     gapi.auth.authorize(
@@ -32,6 +21,19 @@ filePicker.controller('mainController', function($scope){
     );
     return false;
   }
+
+  function handleAuthResult(authResult) {
+    // console.log("handleAuthResult");
+    if (authResult && !authResult.error) {
+      loadDriveApi();
+    }
+  }
+
+  function loadDriveApi() {
+    // console.log("loadDriveApi")
+    gapi.client.load('drive', 'v3', listFiles);
+  }
+
   function listFiles() {
     var request = gapi.client.drive.files.list({
       'pageSize': 10,
@@ -40,8 +42,11 @@ filePicker.controller('mainController', function($scope){
 
     request.execute(function(resp) {
       var files = resp.files;
+      console.log(files[0]);
       for (var i in files) {
-        $scope.files.push(files[i]);
+        $scope.$apply(function() {
+          $scope.files.push(files[i]);
+        });
       }
       $scope.nextPageToken = resp.nextPageToken;
       // console.log(files);
@@ -58,9 +63,33 @@ filePicker.controller('mainController', function($scope){
       $scope.nextPageToken = resp.nextPageToken;
       var files = resp.files;
       for (var i in files) {
-        $scope.files.push(files[i]);
+        $scope.$apply(function() {
+          $scope.files.push(files[i]);
+        });
+      };
+    });
+  }
+
+  $scope.u = function() {
+    var file = document.getElementById("myFile").files;
+    var uploader = new MediaUploader({
+      file: file[0],
+      token: gapi.auth.getToken().access_token,
+      onComplete: function() {
+        $scope.$apply(function() {
+          $scope.uploadMsg = "Succeeded!"
+        })
+      },
+      onError: function() {
+        $scope.uploadMsg = "Error!"
+      },
+      onProgress: function() {
+        $scope.$apply(function() {
+          $scope.uploadMsg = "Uploading.."
+        })
       }
-    })
+    });
+    uploader.upload();
   }
 
 })
